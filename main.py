9bc26e3f539bd089d7f35e96b04c6836e5f7f7b8
Nikodem Majzner <nikodemmajzner@gmail.com>
2022-10-12 08:29:52 +0200

    GAME WORKS!

--- main.py
+++ main.py
@@ -1,16 +1,114 @@
-# This is a sample Python script.
+import random
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+options = ["JAKIE ZERO DEBILU", "kamień", "papier", "nożyce"]
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+playMode = input("1. Graj z graczem, 2. Graj z komputerem, 3. Komputer vs Komputer")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if playMode=="1":
+
+    noWinner = True
+
+    while noWinner:
+        ou1 = int(input("Kamień 1, Papier 2, Nożyce 3"))
+        ou2 = int(input("Kamień 1, Papier 2, Nożyce 3"))
+
+        if ou1 == ou2:
+            print(f"Remis, G1-{options[ou1]} : G2-{options[ou2]}")
+        elif ou2 == 1 and ou1 == 3:
+            print(f"Wygrywa gracz 2, G1-{options[ou1]} : G2-{options[ou2]}")
+            noWinner = False
+        elif ou2 == 1 and ou1 == 2:
+            print(f"Wygrywa gracz 1, G1-{options[ou1]} : G2-{options[ou2]}")
+            noWinner = False
+        elif ou2 == 2 and ou1 == 1:
+            print(f"Wygrywa gracz 2, G1-{options[ou1]} : G2-{options[ou2]}")
+            noWinner = False
+        elif ou2 == 2 and ou1 == 3:
+            print(f"Wygrywa gracz 1, G1-{options[ou1]} : G2-{options[ou2]}")
+            noWinner = False
+        elif ou2 == 3 and ou1 == 1:
+            print(f"Wygrywa gracz 1, G1-{options[ou1]} : G2-{options[ou2]}")
+            noWinner = False
+        elif ou2 == 2 and ou1 == 1:
+            print(f"Wygrywa gracz 2, G1-{options[ou1]} : G2-{options[ou2]}")
+            noWinner = False
+        elif ou2 == 1 and ou1 == 2:
+            print(f"Wygrywa gracz 1, G1-{options[ou1]} : G2-{options[ou2]}")
+            noWinner = False
+        elif ou2 == 3 and ou1 == 2:
+            print(f"Wygrywa gracz 2, G1-{options[ou1]} : G2-{options[ou2]}")
+            noWinner = False
+
+if playMode=="2":
+
+    noWinner = True
+
+    while noWinner:
+        ou1 = int(input("Kamień 1, Papier 2, Nożyce 3"))
+        ou2 = random.randrange(0,3)
+
+        if ou1 == ou2:
+            print(f"Remis, G1-{options[ou1]} : K-{options[ou2]}")
+        elif ou2 == 1 and ou1 == 3:
+            print(f"Wygrywa Komputer, G1-{options[ou1]} : K-{options[ou2]}")
+            noWinner = False
+        elif ou2 == 1 and ou1 == 2:
+            print(f"Wygrywa gracz, G1-{options[ou1]} : K-{options[ou2]}")
+            noWinner = False
+        elif ou2 == 2 and ou1 == 1:
+            print(f"Wygrywa Komputer, G1-{options[ou1]} : K-{options[ou2]}")
+            noWinner = False
+        elif ou2 == 2 and ou1 == 3:
+            print(f"Wygrywa gracz, G1-{options[ou1]} : K-{options[ou2]}")
+            noWinner = False
+        elif ou2 == 3 and ou1 == 1:
+            print(f"Wygrywa gracz, G1-{options[ou1]} : K-{options[ou2]}")
+            noWinner = False
+        elif ou2 == 2 and ou1 == 1:
+            print(f"Wygrywa Komputer, G1-{options[ou1]} : K-{options[ou2]}")
+            noWinner = False
+        elif ou2 == 1 and ou1 == 2:
+            print(f"Wygrywa gracz, G1-{options[ou1]} : K-{options[ou2]}")
+            noWinner = False
+        elif ou2 == 3 and ou1 == 2:
+            print(f"Wygrywa Komputer, G1-{options[ou1]} : K-{options[ou2]}")
+            noWinner = False
+
+if playMode=="3":
+
+    noWinner = True
+
+    while noWinner:
+        ou1 = random.randrange(0,3)
+        ou2 = random.randrange(0,3)
+
+        if ou1 == ou2:
+            print(f"Remis, G1-{options[ou1]} : G2-{options[ou2]}")
+        elif ou2 == 1 and ou1 == 3:
+            print(f"Wygrywa gracz 2, G1-{options[ou1]} : G2-{options[ou2]}")
+            noWinner = False
+        elif ou2 == 1 and ou1 == 2:
+            print(f"Wygrywa gracz 1, G1-{options[ou1]} : G2-{options[ou2]}")
+            noWinner = False
+        elif ou2 == 2 and ou1 == 1:
+            print(f"Wygrywa gracz 2, G1-{options[ou1]} : G2-{options[ou2]}")
+            noWinner = False
+        elif ou2 == 2 and ou1 == 3:
+            print(f"Wygrywa gracz 1, G1-{options[ou1]} : G2-{options[ou2]}")
+            noWinner = False
+        elif ou2 == 3 and ou1 == 1:
+            print(f"Wygrywa gracz 1, G1-{options[ou1]} : G2-{options[ou2]}")
+            noWinner = False
+        elif ou2 == 2 and ou1 == 1:
+            print(f"Wygrywa gracz 2, G1-{options[ou1]} : G2-{options[ou2]}")
+            noWinner = False
+        elif ou2 == 1 and ou1 == 2:
+            print(f"Wygrywa gracz 1, G1-{options[ou1]} : G2-{options[ou2]}")
+            noWinner = False
+        elif ou2 == 3 and ou1 == 2:
+            print(f"Wygrywa gracz 2, G1-{options[ou1]} : G2-{options[ou2]}")
+            noWinner = False
+
+
+
